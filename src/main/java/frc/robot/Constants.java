29fb9f0dce7308d3277bfbe9e97ae54f5dff48e3
Student <student@email>
2024-02-15 18:32:47 -0600

    added LED functionality

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.HashMap;
+
 import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.math.geometry.Translation2d;
@@ -27,12 +29,12 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 2;//4.8 * 2;
-    public static final double kMaxAngularSpeed = 4 * Math.PI; //2*pi radians per second
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
-    public static final double kDirectionSlewRate = 5.0;//1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 5.0;//1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 6.0;//4.0;//2.0; // percent per second (1 = 100%)
+    public static final double kDirectionSlewRate = 1.2; // radians per second
+    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(20);
@@ -165,45 +167,46 @@ public final class Constants {
     public static final int FEEDER_BEAM_BREAK = 0;
   }
 
-  public static final class LEDs{
+  public static final class LEDs {
     public static final int purple_Red = 148;
     public static final int purple_Green = 0;
     public static final int purple_Blue = 211;
-    public static final int purple_Hue = 282;
+    public static final int purple_Hue =  282;
     public static final int purple_Sat = 100;
     public static final int purple_Val = 83;
 
     public static final int yellow_Red = 255;
     public static final int yellow_Green = 90;
     public static final int yellow_Blue = 0;
-    public static final int yellow_Hue = 21;
+    public static final int yellow_Hue =  21;
     public static final int yellow_Sat = 100;
     public static final int yellow_Val = 100;
 
     public static final int orange_Red = 251;
     public static final int orange_Green = 51;
     public static final int orange_Blue = 0;
-    public static final int orange_Hue = 12;
+    public static final int orange_Hue =  12;
     public static final int orange_Sat = 100;
     public static final int orange_Val = 98;
 
-    public static final int red_Hue = 0;
+    public static final int red_Hue =  0;
     public static final int red_Sat = 100;
     public static final int red_Val = 100;
 
-    public static final int blue_Hue = 240;
-    public static final int blue_Sat = 100;
-    public static final int blue_Val = 100;
-
-    public static final int green_Hue = 240;
+    public static final int green_Hue =  120;
     public static final int green_Sat = 100;
     public static final int green_Val = 100;
+
+    public static final int blue_Hue =  240;
+    public static final int blue_Sat = 100;
+    public static final int blue_Val = 100;
 
     public static final int Red = 255;
     public static final int Green = 255;
     public static final int Blue = 255;
-    public static final int white_Hue = 0;
+    public static final int white_Hue =  0;
     public static final int white_Sat = 0;
     public static final int white_Val = 100;
+
   }
 }
