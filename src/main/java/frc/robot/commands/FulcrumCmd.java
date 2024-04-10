c9e166e9e260f7d8f29dde26152f9fd38f0cf0bb
chadr03@gmail.com <chadr03@gmail.com>
2024-04-09 20:29:54 -0500

    change bottom setpoint

--- src/main/java/frc/robot/commands/FulcrumCmd.java
+++ src/main/java/frc/robot/commands/FulcrumCmd.java
@@ -31,7 +31,7 @@ public class FulcrumCmd extends Command {
                 this.fulcrum.setSetPoint(54.1);
                 break;
             case INTAKE:
-                this.fulcrum.setSetPoint(12);
+                this.fulcrum.setSetPoint(13);
                 break;
             case AMP:
                 this.fulcrum.setSetPoint(90);
